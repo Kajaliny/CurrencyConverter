b2dbcd06f076aea4eb1698c422fe3d678adb0457
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2025-01-02 17:19:49 +0000

    result.js component created successfully

---
+++ currency-converter/src/componenents/Result.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Result = ({ convertedAmount }) => {
+  return (
+    <div>
+      <h3>Converted Amount: {convertedAmount}</h3>
+    </div>
+  );
+};
+
+export default Result;
